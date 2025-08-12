650e0800920d74f80ce2fc4d81e5d047745e40a2
kmlee96 <jaoming787@gmail.com>
2025-08-12 09:52:44 +0900

    start llm

---
+++ gpt_basic.py
@@ -0,0 +1,22 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+api_key = os.getenv("OPEN_API_KEY")
+client = OpenAI(api_key=api_key)
+
+response = client.chat.completions.create(
+    model="gpt-4o",
+    temperature=0.1,
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "질문"},
+    ]
+)
+
+print(response)
+
+print("----")
+print(response.choices[0].message.content)
