eefa8fd61f2025b2d90b5608efd0166b11f9abc1
kmlee96 <jaoming787@gmail.com>
2025-08-12 10:38:19 +0900

    prompt engineer

---
+++ few_shot_prompt.py
@@ -0,0 +1,27 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+
+client = OpenAI(api_key=api_key)
+
+response = client.chat.completions.create(
+    model="gpt-4o",
+    temperature=0.9,
+    messages=[
+        {"role": "system", "content": "you are an animal sound creator"},
+        {"role": "user", "content": "dog"},
+        {"role": "assistant", "content": "bow wow"},
+        {"role": "user", "content": "cat"},
+        {"role": "assistant", "content": "meow"},
+        {"role": "user", "content": "bird"},
+    ]
+)
+
+print(response)
+
+print("----")
+print(response.choices[0].message.content)
--- gpt_basic.py
+++ gpt_basic.py
@@ -4,7 +4,8 @@ import os
 
 
 load_dotenv()
-api_key = os.getenv("OPEN_API_KEY")
+api_key = os.getenv("OPENAI_API_KEY")
+
 client = OpenAI(api_key=api_key)
 
 response = client.chat.completions.create(
